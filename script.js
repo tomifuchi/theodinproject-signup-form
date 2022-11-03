b9f8041ec50263ec9f4d4b52f23028de59f16ccd
tomifuchi <tomifuchi@yahoo.com>
2022-11-03 22:27:22 +0700

    Polishing state

---
+++ script.js
@@ -0,0 +1,46 @@
+//Client side validation
+
+//The form itself
+const registration_form = document.getElementById('registration');
+registration_form.addEventListener('submit', validateForm);
+
+const user_email = document.getElementById('user_email');
+const user_pnumber = document.getElementById('user_pnumber');
+
+user_email.addEventListener('change', () => {
+    user_email.classList.remove('initial');
+});
+
+if (user_email.value != ''){
+    user_email.classList.remove('initial');
+}
+
+user_pnumber.addEventListener('change', () => {
+    user_pnumber.classList.remove('initial');
+});
+
+if (user_pnumber.value != ''){
+    user_pnumber.classList.remove('initial');
+}
+
+function validateForm(e) {
+    //This just check the password to see if they are identical, else we don't submit
+    //The other invalids are checked by the browser engine itself so no need
+    //Password
+    const user_pwd_input = document.getElementById('user_pwd');
+    const user_confirm_pwd_input = document.getElementById('user_confirm_pwd');
+
+    user_pwd_input.classList.remove('initial');
+    user_confirm_pwd_input.classList.remove('initial');
+    user_email.classList.remove('initial');
+    user_pnumber.classList.remove('initial');
+
+    if(!(user_pwd_input.value == user_confirm_pwd_input.value)) {
+        user_pwd_input.classList.add('error-confirm');
+        user_confirm_pwd_input.classList.add('error-confirm');
+        e.preventDefault();
+    } else if (user_pwd_input.value == user_confirm_pwd_input.value) {
+        user_pwd_input.classList.remove('error-confirm');
+        user_confirm_pwd_input.classList.remove('error-confirm');
+    }
+}
